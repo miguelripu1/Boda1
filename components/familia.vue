aeb9931930e7febdd6b0fc3ff20a3281f04bb1a3
miguelripu1 <miguel.rios@davivienda.com>
2024-08-26 15:13:42 -0500

    Agregar cambios

---
+++ components/familia.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="familia-section">
+    <div class="container py-5">
+      <h1 class="text-center mb-4 titulo-familia">Con la bendición de Dios y de nuestros padres</h1>
+      <div class="row p-5">
+        <div class="col-md-6 mb-3">
+          <img :src="`../img/familia/familia_esposa.png`" alt="Familia 1" class="img-fluid rounded">
+          <p class="text-center mt-2"style="color:red;">Juan Alberto Robayo Corredor y Alba Margoth Gomez Malgón</p>
+        </div>
+        <div class="col-md-6 mb-3">
+          <img :src="`../img/familia/familia_esposo.png`" alt="Familia 2" class="img-fluid rounded">
+          <p class="text-center mt-2">Miguel Arcángel Rios Pulido y Rosa Maria Pulido Rodriguez</p>
+        </div>
+      </div>
+      <div class="bendicion-text">
+        <p class="text-center mt-4">
+          La bendición de nuestros padres y seres queridos es invaluable. Su apoyo, sabiduría y amor incondicional nos han guiado hasta este momento. Llevamos con nosotros su bendición y aspiramos a honrar su legado construyendo un camino lleno de amor, respeto y comprensión mutua.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+.titulo-familia{
+  font-family: Parisienne;
+  font-size: 32px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  color: #28464E;
+}
+.familia-section {
+  background: linear-gradient(to bottom, #ffffff 50%, #f8f9fa 50%);
+}
+
+.bendicion-text{
+  font-size: 1.25rem;
+}
+@media (max-width: 767px) {
+  .familia-section {
+    background: linear-gradient(to bottom, #ffffff 60%, #f8f9fa 40%);
+  }
+}
+</style>
---
+++ components/mesaRegalos.vue
@@ -0,0 +1,111 @@
+<template>
+<h2 class="section-title">Mesa de regalos</h2>
+  <div class="container my-5">
+    <div class="row">
+      <div id="textoRegalos" class="col-12 col-lg-6 ">
+        <p class="description">
+          El mejor regalo que podríamos recibir es contar con tu presencia en nuestro día especial. Sin embargo, si deseas contribuir de otra manera, una lluvia de sobres será más que bienvenida.
+        </p>
+        <p class="description-second">
+         ¡Gracias por ser parte de este momento tan importante en nuestras vidas!
+        </p>
+      </div>
+      <div class="col-12 col-lg-6 text-center " style="margin-top: 0px;">
+         <div class="col-md-11">
+          <div class="card border-0" id="card-lluvia">
+            <div class="card-body d-flex flex-row align-items-center">
+              <img :src="`../img/mesa/sobre.png`" alt="Sobre" class="img-fluid" id="img-Sobre">
+              <h5 class="ml-3 lluvia-text">Lluvia de sobres</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // JavaScript
+};
+</script>
+
+<style scoped>
+#textoRegalos{
+  /*padding: 0 10%;*/
+}
+.section-title {
+  margin-top: 30px;
+  margin-bottom: 30px;
+  font-family: Cardo;
+  font-size: 32px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  color:#593E25;
+  padding-left: 60px;
+  margin-bottom: 0px;
+}
+.lluvia-text{
+  font-family: "Gudea", Sans-serif;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  color:#347355;
+}
+.text-center{
+  margin-top:30px;
+
+}
+.description {
+  margin-bottom: 0.5rem;
+  font-family: "Gudea", sans-serif;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  color: #593E25;
+}
+.description-second {
+  margin-bottom: 0.5rem;
+  font-family: "Gudea", sans-serif;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  color: #593E25;
+}
+
+.card-body {
+  background-color: #E1EAE6; 
+  color: #ffffff;
+  justify-content: center;
+  border-radius: 10px;
+}
+
+#card-lluvia{
+  background-color: #D7E6F2;
+}
+
+#img-Sobre{
+  width: 15%;
+}
+
+@media (max-width: 991px) {
+  /* Ajustes para pantallas pequeñas */
+  .section-title {
+    background-color: #fff;
+    padding-left: 10px;
+    width: fit-content;
+    margin-bottom: 0px;
+  }
+  .description {
+    text-align: justify;
+  }
+  .description-second{
+    margin-bottom: 30px;
+    text-align: justify;
+  }
+}
+</style>
